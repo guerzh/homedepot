55254be34b02e4a7ac8c7403c7767283d933a04a
Michael Guerzhoy <guerzh@gmail.com>
2016-02-08 01:08:44 -0500

    removed attr loading since there are multiple attribute per product_uid

--- hd1.py
+++ hd1.py
@@ -8,10 +8,10 @@ os.chdir("/home/guerzhoy/Desktop/homedepot")
 descs = read_csv("product_descriptions.csv")
 train = read_csv("train.csv")
 test = read_csv("test.csv")
-attr = read_csv("attributes.csv")
 
-train = train.merge(descs, on=["product_uid"]).merge(attr, on="product_uid")
-test = test.merge(descs, on=["product_uid"]).merge(attr, on="product_uid")
+
+train = train.merge(descs, on=["product_uid"])#.merge(attr, on="product_uid")
+test = test.merge(descs, on=["product_uid"])#.merge(attr, on="product_uid")
 
 
 
@@ -44,16 +44,16 @@ def get_other_brand_present(search_terms, product_title, brands):
     else:
         return 0
     
-skip = 50
+skip = 10
 
-train_learn_x = zeros((len(train.index)/skip,10))
+train_learn_x = zeros((len(train.index)/skip,6))
 train_learn_y = zeros((len(train.index)/skip,1))
 
 brands = set([a.split()[0].lower() for a in train["product_title"]])
 
 
 for i in range(0, len(train.index), skip):
-    if i % 10000 == 0:
+    if i % 1000 == 0:
         print i
     line = train.loc[i,:]
     
@@ -61,8 +61,8 @@ for i in range(0, len(train.index), skip):
         search_terms = [a.strip() for a in line["search_term"].lower().split()]
         product_title = [a.strip() for a in line["product_title"].lower().split()]
         product_desc = [a.strip() for a in line["product_description"].lower().split()]
-        name = [a.strip() for a in line["name"].lower().split()]
-        attr = [a.strip() for a in line["value"].lower().split()]
+        
+        
         
         relevance = float(line["relevance"])
         
@@ -71,12 +71,12 @@ for i in range(0, len(train.index), skip):
         
         train_learn_x[i/skip,:] = array((get_cosine_similarity(search_terms, product_title),
                                 get_cosine_similarity(search_terms, product_desc),
-                                get_cosine_similarity(search_terms, name),
-                                get_cosine_similarity(search_terms, attr),
+                                
+                                
                                 get_overlap(search_terms, product_title),
                                 get_overlap(search_terms, product_desc),
-                                get_overlap(search_terms, name),
-                                get_overlap(search_terms, attr),
+                                
+                                
                                 brand_present,
                                 other_brand_present))
     
@@ -93,7 +93,13 @@ for i in range(0, len(train.index), skip):
 
 from sklearn import datasets, linear_model
 
-regr = linear_model.Ridge(alpha=.0001)
+#Ridge regression:
+#
+# minimize (SUM_i (y_i - (a0+a1*xi1+a2*xi2+...ak*xik))^2)  + alpha*|a|^2 )
+#
+
+
+regr = linear_model.Ridge(alpha=.00001)
 regr.fit(train_learn_x, train_learn_y)
 print 'Coefficients: \n', regr.coef_
 #the coefficients for the eight features computed in lines 52-59
