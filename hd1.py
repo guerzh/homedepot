65994baea44263d3ea5325066b1564c987eb0adc
Michael Guerzhoy <guerzh@gmail.com>
2016-02-12 19:50:21 -0500

    generate predictions for the test set

--- hd1.py
+++ hd1.py
@@ -5,13 +5,6 @@ from numpy import *
 
 os.chdir("/home/guerzhoy/Desktop/homedepot")
 
-descs = read_csv("product_descriptions.csv")
-train = read_csv("train.csv")
-test = read_csv("test.csv")
-
-
-train = train.merge(descs, on=["product_uid"])#.merge(attr, on="product_uid")
-test = test.merge(descs, on=["product_uid"])#.merge(attr, on="product_uid")
 
 
 
@@ -27,7 +20,7 @@ def get_cosine_similarity(doc1, doc2):
     return float(overlap)/(len(doc1)*len(doc2))
     
     
-def get_brand_present(seach_terms, product_title):
+def get_brand_present(search_terms, product_title):
     if product_title[0] in search_terms:
         return 1
     else:
@@ -44,49 +37,63 @@ def get_other_brand_present(search_terms, product_title, brands):
     else:
         return 0
     
-skip = 1
-
-train_learn_x = zeros((len(train.index)/skip,6))
-train_learn_y = zeros((len(train.index)/skip,1))
-
-brands = set([a.split()[0].lower() for a in train["product_title"]])
 
 
-for i in range(0, len(train.index), skip):
-    if i % 1000 == 0:
-        print i
-    line = train.loc[i,:]
+def get_features(test=False, skip=1):
+    if not test:
+        train = read_csv("train.csv")
+    else:
+        train = read_csv("test.csv")
+    descs = read_csv("product_descriptions.csv")
+    train = train.merge(descs, on=["product_uid"])#.merge(attr, on="product_uid")
     
-    try:
-        search_terms = [a.strip() for a in line["search_term"].lower().split()]
-        product_title = [a.strip() for a in line["product_title"].lower().split()]
-        product_desc = [a.strip() for a in line["product_description"].lower().split()]
-        
-        
-        
-        relevance = float(line["relevance"])
-        
-        brand_present = get_brand_present(search_terms, product_title)
-        other_brand_present = get_other_brand_present(search_terms, product_title, brands)
-        
-        train_learn_x[i/skip,:] = array((get_cosine_similarity(search_terms, product_title),
-                                get_cosine_similarity(search_terms, product_desc),
-                                
-                                
-                                get_overlap(search_terms, product_title),
-                                get_overlap(search_terms, product_desc),
-                                
-                                
-                                brand_present,
-                                other_brand_present))
     
-        train_learn_y[i/skip,:] = relevance
-    except:
-        print "ERROR", i
-        #train_learn_x[i,:] = zeros((1, 4))
-        #train_learn_y[i,0] = 0
+    train_learn_x = zeros((len(train.index)/skip,6))
+    train_learn_y = zeros((len(train.index)/skip,1))
+    
+    brands = set([a.split()[0].lower() for a in train["product_title"]])
+    
+    
+    for i in range(0, len(train.index), skip):
+        if i % 1000 == 0:
+            print i
+        line = train.loc[i,:]
         
-
+        try:
+            search_terms = [a.strip() for a in line["search_term"].lower().split()]
+            product_title = [a.strip() for a in line["product_title"].lower().split()]
+            product_desc = [a.strip() for a in line["product_description"].lower().split()]
+            
+            
+            
+            
+            
+            brand_present = get_brand_present(search_terms, product_title)
+            other_brand_present = get_other_brand_present(search_terms, product_title, brands)
+            
+            train_learn_x[i/skip,:] = array((get_cosine_similarity(search_terms, product_title),
+                                    get_cosine_similarity(search_terms, product_desc),
+                                    
+                                    
+                                    get_overlap(search_terms, product_title),
+                                    get_overlap(search_terms, product_desc),
+                                    
+                                    
+                                    brand_present,
+                                    other_brand_present))
+            if not test:
+                relevance = float(line["relevance"])
+                train_learn_y[i/skip,:] = relevance
+        except:
+            print "ERROR", i
+            #train_learn_x[i,:] = zeros((1, 4))
+            #train_learn_y[i,0] = 0
+        
+    
+    if not test:
+        return train_learn_x, train_learn_y
+    else:
+        return train_learn_x
 
 
 
@@ -97,12 +104,18 @@ from sklearn import datasets, linear_model
 #
 # minimize (SUM_i (y_i - (a0+a1*xi1+a2*xi2+...ak*xik))^2)  + alpha*|a|^2 )
 #
-
+train_learn_x, train_learn_y = get_features(test=False, skip=1)
 
 regr = linear_model.Ridge(alpha=.00001)
 regr.fit(train_learn_x, train_learn_y)
 print 'Coefficients: \n', regr.coef_
 #the coefficients for the eight features computed in lines 52-59
+
+
+def bracket(arr, min_val, max_val):
+    return arr
+    return minimum(maximum(arr, min_val), max_val)
+
 
 print "RMSE: %f" % sqrt(mean((regr.predict(train_learn_x) - train_learn_y) ** 2)) 
 print "Baseline: %f" % sqrt(mean((mean(train_learn_y) - train_learn_y) ** 2))      
@@ -110,15 +123,26 @@ print "Baseline: %f" % sqrt(mean((mean(train_learn_y) - train_learn_y) ** 2))
 from sklearn import cross_validation
 import time
 
+
+
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(train_learn_x, train_learn_y, test_size=0.4, random_state=int(time.time()))
 regr = linear_model.Ridge(alpha=.00001)
 regr.fit(X_train, y_train)
 print "Train RMSE: %f" % sqrt(mean((regr.predict(X_train) - y_train) ** 2))  
-print "Train Baseline: %f" % sqrt(mean((mean(y_train) - X_train) ** 2))      
+print "Train Baseline: %f" % sqrt(mean((mean(y_train) - y_train) ** 2))      
 print "Test RMSE: %f" % sqrt(mean((regr.predict(X_test) - y_test) ** 2))  
-print "Test Baseline: %f" % sqrt(mean((mean(y_test) - X_test) ** 2))      
+print "Test Baseline: %f" % sqrt(mean((mean(y_train) - y_test) ** 2))      
 
 
 
 
+test = read_csv("test.csv")
+test_feat = get_features(test=True, skip=1)
+test_y = bracket(regr.predict(test_feat),1,3)
+test_y = minimum(3, test_y)
+test_y = maximum(1, test_y)
+
+test["relevance"] = test_y
+
+test.loc[:,["id", "relevance"]].to_csv("pred.csv", index=False)
 
